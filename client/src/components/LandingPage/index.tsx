856ebd8a24c0683bc86562272af2035ffb5e65e7
FrancoLedi <francoivanledesma551@gmail.com>
2023-02-06 18:44:16 -0300

    LandingPage terminada

--- client/src/components/LandingPage/index.tsx
+++ client/src/components/LandingPage/index.tsx
@@ -1,17 +1,33 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Delorean from '../../img/Delorean.mp4';
+import StartIconLight from '../../img/StartIconLight.png';
+import StartIconDark from '../../img/StartIconDark.png';
 import '../../index.css';
 
 export default function LandingPage(){
+    const [isHover, setIsHover] = useState(false);
     return (
         <>
         <div className='letter'>
-            <h1>¡Welcome gamers!</h1>
+            <h1 className='Title'>¡Welcome gamers!</h1>
             <Link to = '/home'>
-                <button className='button'>
+                <button className='StartButton' onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
+                    {
+                        isHover ? 
+                        <img className='Start' src={StartIconDark} />
+                     :  <img className='Start' src={StartIconLight} />
+                    }
                     Press Start
                 </button>
             </Link>
         </div>
+
+            <div className='VideoContainer'>
+                <video loop autoPlay muted>
+                    <source src={Delorean} type="video/mp4" />
+                </video>
+            </div>
         </>
     )
 }
--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="react-scripts" />
+declare module "*.mp4"; // Agregado manualmente para poder importar video mp4
