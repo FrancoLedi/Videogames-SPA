ca8e9e0dbcf813c77ab861b52a9eee7a025a01b4
FrancoLedi <francoivanledesma551@gmail.com>
2023-02-20 20:11:21 -0300

    Another try

--- client/src/components/Filters/index.tsx
+++ client/src/components/Filters/index.tsx
@@ -80,7 +80,7 @@ const Filters: React.FunctionComponent<PropsFilter> = ({paginado, refreshCompone
             <Select options={options()}
             defaultValue={[{label: 'Genres'}]} 
             styles={selectStyles}
-            onChange={(value: SelectOption) => handleFilterBy(value)}
+            onChange={(value) => handleFilterBy(value!)}
             
             />
 
